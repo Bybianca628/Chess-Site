1191db1c6602d864141de87194d01b10a1f795a9
Bybianca628 <bi.oliveira@ime.usp.br>
2024-11-09 22:38:39 -0300

    added objects for the balck and white players, and a function that print pieces, if they are not captured

---
+++ scripts.js
@@ -0,0 +1,261 @@
+const White = {
+    time: 0 ,
+    pieces: {
+        aPawn: {
+            id: 'wap',
+			file: 'contents/images/pB.png'  ,
+			alt: 'white pawn',
+			isCaptured: false ,
+            position: 'a2',
+        },
+        bPawn: {
+            id: 'wbp',
+			file: 'contents/images/pB.png'  ,
+			alt: 'white pawn',
+			isCaptured: false ,
+            position: 'b2',
+        },
+        cPawn: {
+            id: 'wcp',
+			file: 'contents/images/pB.png'  ,
+			alt: 'white pawn',
+			isCaptured: false ,
+            position: 'c2',
+        },
+        dPawn: {
+            id: 'wdp',
+			file: 'contents/images/pB.png'  ,
+			alt: 'white pawn',
+			isCaptured: false ,
+            position: 'd2',
+        },
+        ePawn: {
+            id: 'wep',
+			file: 'contents/images/pB.png'  ,
+			alt: 'white pawn',
+			isCaptured: false ,
+            position: 'e2',
+        },
+        fPawn: {
+            id: 'wfp',
+			file: 'contents/images/pB.png'  ,
+			alt: 'white pawn',
+			isCaptured: false ,
+            position: 'f2',
+        },
+        gPawn: {
+            id: 'wgp',
+			file: 'contents/images/pB.png'  ,
+			alt: 'white pawn',
+			isCaptured: false ,
+            position: 'g2',
+        },
+        hPawn: {
+            id: 'whp',
+			file: 'contents/images/pB.png'  ,
+			alt: 'white pawn',
+			isCaptured: false ,
+            position: 'h2',
+        },
+        qRook: {
+            id: 'wqr',
+			file: 'contents/images/rB.png'  ,
+			alt: 'white rook',
+			isCaptured: false ,
+            position: 'a1',
+        },
+        qKnight: {
+            id: 'wqn',
+			file: 'contents/images/nB.png'  ,
+			alt: 'white knight',
+			isCaptured: false ,
+            position: 'b1',
+        },
+        dBishop: {
+            id: 'wdb',
+			file: 'contents/images/bB.png'  ,
+			alt: 'white bishop',
+			isCaptured: false ,
+            position: 'c1',
+        },
+        Queen: {
+            id: 'wq',
+			file: 'contents/images/qB.png'  ,
+			alt: 'white queen',
+			isCaptured: false ,
+            position: 'd1',
+        },
+        King:{
+            id: 'wk',
+			file: 'contents/images/kB.png'  ,
+			alt: 'white king',
+			isCaptured: false ,
+            position: 'e1',
+        },
+        lBishop: {
+            id: 'wlb',
+			file: 'contents/images/bB.png'  ,
+			alt: 'white bishop',
+			isCaptured: false ,
+            position: 'f1',
+        },
+        kKnight: {
+            id: 'wkn',
+			file: 'contents/images/nB.png'  ,
+			alt: 'white knight',
+			isCaptured: false ,
+            position: 'g1',
+        },
+        kRook:{
+            id: 'wkr',
+			file: 'contents/images/rB.png'  ,
+			alt: 'white rook',
+			isCaptured: false ,
+            position: 'h1',
+        },
+    },
+}
+
+const Black = {
+    time: 0 ,
+    pieces:{
+        aPawn: {
+            id: 'bap',
+			file: 'contents/images/pP.png'  ,
+			alt: 'black pawn',
+			isCaptured: false ,
+            position: 'a7',
+        },
+        bPawn: {
+            id: 'bbp',
+			file: 'contents/images/pP.png'  ,
+			alt: 'black pawn',
+			isCaptured: false ,
+            position: 'b7',
+        },
+        cPawn: {
+            id: 'bcp',
+			file: 'contents/images/pP.png'  ,
+			alt: 'black pawn',
+			isCaptured: false ,
+            position: 'c7',
+        },
+        dPawn: {
+            id: 'bdp',
+			file: 'contents/images/pP.png'  ,
+			alt: 'black pawn',
+			isCaptured: false ,
+            position: 'd7',
+        },
+        ePawn: {
+            id: 'bep',
+			file: 'contents/images/pP.png'  ,
+			alt: 'black pawn',
+			isCaptured: false ,
+            position: 'e7',
+        },
+        fPawn: {
+            id: 'bfp',
+			file: 'contents/images/pP.png'  ,
+			alt: 'black pawn',
+			isCaptured: false ,
+            position: 'f7',
+        },
+        gPawn: {
+            id: 'bgp',
+			file: 'contents/images/pP.png'  ,
+			alt: 'black pawn',
+			isCaptured: false ,
+            position: 'g7',
+        },
+        hPawn: {
+            id: 'bhp',
+			file: 'contents/images/pP.png'  ,
+			alt: 'black pawn',
+			isCaptured: false ,
+            position: 'h7',
+        },
+        qRook: {
+            id: 'bqr',
+			file: 'contents/images/rP.png'  ,
+			alt: 'black rook',
+			isCaptured: false ,
+            position: 'a8',
+        },
+        qKnight: {
+            id: 'bqk',
+			file: 'contents/images/nP.png'  ,
+			alt: 'black knight',
+			isCaptured: false ,
+            position: 'b8',
+        },
+        dBishop: {
+            id: 'bdb',
+			file: 'contents/images/bP.png'  ,
+			alt: 'black bishop',
+			isCaptured: false ,
+            position: 'f8',
+        },
+        Queen: {
+            id: 'bq',
+			file: 'contents/images/qP.png'  ,
+			alt: 'black queen',
+			isCaptured: false ,
+            position: 'd8',
+        },
+        King:{
+            id: 'bk',
+			file: 'contents/images/kP.png'  ,
+			alt: 'black king',
+			isCaptured: false ,
+            position: 'e8',
+        },
+        lBishop: {
+            id: 'blb',
+			file: 'contents/images/bP.png'  ,
+			alt: 'black bishop',
+			isCaptured: false ,
+            position: 'c8',
+        },
+        kKnight: {
+            id: 'bkn',
+			file: 'contents/images/nP.png'  ,
+			alt: 'black knight',
+			isCaptured: false ,
+            position: 'g8',
+        },
+        kRook:{
+            id: 'bkr',
+			file: 'contents/images/rP.png'  ,
+			alt: 'black rook',
+			isCaptured: false ,
+            position: 'h8',
+        },
+    },
+}
+
+function printPieces(){
+    console.log("Função sendo executada");
+    for (piece in White.pieces){
+        console.log("Entrou no loop");
+        console.log(White.pieces[piece]);
+        if (!White.pieces[piece].isCaptured){
+            imgText= '<img src="'+ White.pieces[piece].file +'" alt="' + White.pieces[piece].alt + '" id="' + White.pieces[piece].id + '" class="full"/>'
+            console.log(imgText);
+            console.log(White.pieces[piece].position);
+            document.getElementsByClassName(White.pieces[piece].position)[0].innerHTML =  imgText;
+            imgtext='';
+        }
+    }
+    for (piece in Black.pieces){
+        console.log("Entrou no loop");
+        console.log(Black.pieces[piece]);
+        if (!Black.pieces[piece].isCaptured){
+            imgText= '<img src="'+ Black.pieces[piece].file +'" alt="' + White.pieces[piece].alt + '" id="' + White.pieces[piece].id + '" class="full"/>'
+            console.log(imgText);
+            console.log(Black.pieces[piece].position);
+            document.getElementsByClassName(Black.pieces[piece].position)[0].innerHTML =  imgText;
+            imgtext='';
+        }
+    }
+}
